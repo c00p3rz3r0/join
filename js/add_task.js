c4953808eda1745fefdf211f1c6cf5a93b5ef33d
derKanzlerin <alexander.bahr86@gmx.de>
2023-10-23 21:44:15 +0800

    add task final templaten+localstorage

--- js/add_task.js
+++ js/add_task.js
@@ -33,3 +33,47 @@ function loadAllTask() {
     let allTasksAsString = localStorage.getItem('allTasks');
     allTasks = JSON.parse(allTasksAsString);
 }
+
+function addSubTask() {
+    const taskInput = document.getElementById('task-input');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const taskList = document.getElementById('task-list');
+
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('input-group', 'mb-3');
+
+        const inputGroupText = document.createElement('div');
+        inputGroupText.classList.add('input-group-text');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.classList.add('form-check-input', 'mt-0');
+        checkbox.value = '';
+        checkbox.setAttribute('aria-label', 'Checkbox for following text input');
+
+        const taskInputField = document.createElement('input');
+        taskInputField.type = 'text';
+        taskInputField.classList.add('form-control');
+        taskInputField.setAttribute('aria-label', 'Text input with checkbox');
+        taskInputField.value = taskText;
+
+        const deleteButton = document.createElement('span');
+        deleteButton.textContent = '❌';
+        deleteButton.classList.add('input-group-text', 'delete-button');
+        deleteButton.addEventListener('click', () => {
+            taskList.removeChild(taskItem);
+        });
+
+        inputGroupText.appendChild(checkbox);
+        taskItem.appendChild(inputGroupText);
+        taskItem.appendChild(taskInputField);
+        taskItem.appendChild(deleteButton);
+
+        taskList.appendChild(taskItem);
+        taskInput.value = '';
+    }
+}
+
+
