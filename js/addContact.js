45bf10479ce83cfbbfee2888d1d64248fd2d5469
c00p3rz3r0 <125402398+c00p3rz3r0@users.noreply.github.com>
2023-10-24 22:40:29 +0200

    Contact, Task

--- js/addContact.js
+++ js/addContact.js
@@ -1,25 +1,33 @@
-
-
 let allContacts = [];
 
-function addContact() {
-    let contactName = document.getElementById('nameInputField');
-    let contactMail = document.getElementById('mailInputField');
-    let contactPhone = document.getElementById('phoneInputField');
+async function initContact(){
+    await loadAllContacts();
+    loadgeneralContacts();
+    loadDetail(0);
+}
 
-    let contact = {
-        'name': contactName.value,
-        'mail': contactMail.value,
-        'phone': contactPhone.value,
-        'contactCreatedAt': new Date().getTime()  // brauchen wir das bei den Kontakten?
-    }
+function initAddContact(){
+    let contactDiv = document.getElementById('newContact');
+    let allContactDiv = document.getElementById('allContact');
+    contactDiv.classList.remove('d-none');
+    allContactDiv.classList.add('d-none');
+}
 
-    allContacts.push(contact);
+async function addContact() {
+    let firstName = document.getElementById('firstnameInputField');
+    let lastName = document.getElementById('lastnameInputField');
+    let email = document.getElementById('mailInputField');
+    let phone = document.getElementById('phoneInputField');
 
-    let allContactsAsString = JSON.stringify(allContacts);
-    localStorage.setItem('allContacts', allContactsAsString);
-
-    deleteInputFields(contactName, contactMail, contactPhone);
+    allContacts.push({
+        firstname: firstName.value,
+        lastname: lastName.value,
+        mail: email.value,
+        phone: phone.value,
+    })
+    await setItem('contact', JSON.stringify(allContacts));
+    cancleNewContact();
+    initContact();
 }
 
 
@@ -31,7 +39,60 @@ function deleteInputFields(contactName, contactMail, contactPhone) {
 
 
 // Load all Contacts
-function loadAllContacts() {
-    let allContactsAsString = localStorage.getItem('allContacts');
-    allContacts = JSON.parse(allContactsAsString);
+async function loadAllContacts() {
+    try{
+        allContacts = JSON.parse(await getItem('contact'));
+    } catch(e){
+        console.error('Loading error: ', e);
+    }
+    
 }
+ function cancleNewContact(){
+    let contactDiv = document.getElementById('newContact');
+    let allContactDiv = document.getElementById('allContact');
+    document.getElementById('firstnameInputField').value='';
+    document.getElementById('lastnameInputField').value='';
+    document.getElementById('mailInputField').value='';
+    document.getElementById('phoneInputField').value='';
+    contactDiv.classList.add('d-none');
+    allContactDiv.classList.remove('d-none');
+ }
+ function loadgeneralContacts(){
+    let contactsDiv = document.getElementById('contacts');
+    contactsDiv.innerHTML = '';
+    for (let index = 0; index < allContacts.length; index++) {
+        const element = allContacts[index];
+            let firstName = element['firstname'];
+            let lastname = element['lastname'];
+            let email = element['mail'];
+            let nameIcon = getShortIcon(index);
+        contactsDiv.innerHTML += `
+        <div onClick="loadDetail(${index})" class="contact-name">
+        <div class="contact-name-circle" id="nameIcon${index}">
+            <img src="assed/svg/contact-imgs/Ellipse 5.svg" alt="" />
+            <div class="contact-name-circle-txt" id="nameIcons${index}" >${nameIcon}</div>
+        </div>
+        <div class="name-mail-div-frame81">
+             <div class="contact-name-txt">${firstName} ${lastname}</div>
+             <div class="contact-email">${email}</div>
+        </div>
+        </div>
+        `;
+    }
+ }
+
+ function loadDetail(index){
+    element = allContacts[index];
+    document.getElementById('nameContact').innerHTML = `${element['firstname']} ${element['lastname']}`;
+    document.getElementById('emailContact').innerHTML = element['mail'];
+    document.getElementById('phoneNumer').innerHTML = element['phone'];
+    document.getElementById('iconDetail').innerHTML = getShortIcon(index);
+ }
+
+ function getShortIcon(index){
+    element = allContacts[index];
+    let firstName = element['firstname'];
+    let lastname = element['lastname'];
+    let nameIcon = `${firstName.at(0)}${lastname.at(0)}`;
+    return nameIcon;
+ }
--- js/add_task.js
+++ js/add_task.js
@@ -1,35 +1,58 @@
 let allTasks = [];
+let allAssigned =[];
 
-function addTask() {
+async function initnewTask(){
+    await loadContacts();
+    loadAssigned();
+    await loadAllTask();
+}
+
+async function loadContacts(){
+    try{
+        allAssigned = JSON.parse(await getItem('contact'));
+    } catch(e){
+        console.error('Loading error: ', e);
+    }
+}
+
+async function loadAssigned() {
+    let assigned = document.getElementById('datalistOptions');
+    assigned.innerHTML = '';
+    for (let index = 0; index < allAssigned.length; index++) {
+        const element = allAssigned[index];
+        let firstName = element['firstname'];
+        let lastname = element['lastname'];
+        assigned.innerHTML += `<option>${firstName} ${lastname}</option>`;
+    }
+}
+
+async function addTask() {
     let taskTitle = document.getElementById('taskTitle');
     let taskDescription = document.getElementById('taskDescription');
     let taskDueDate = document.getElementById('tastDueDate');
     let taskContact = document.getElementById('taskContact');
     let taskCategory = document.getElementById('taskCategory');
 
-    let task = {
-        'title': taskTitle.value,
-        'description': taskDescription.value,
-        'dueDate': taskDueDate.value,
-        'contact': taskContact.value,
-        'category': taskCategory.value,
+    allTasks.push({
+        title: taskTitle.value,
+        description: taskDescription.value,
+        dueDate: taskDueDate.value,
+        //contact: taskContact.value,
+        category: taskCategory.value,
         // 'pro': taskDescription.value,
-        'createdAt': new Date().getTime(),
-    };
-    allTasks.push(task);
-    let allTasksAsString = JSON.stringify(allTasks);
-    localStorage.setItem('allTasks', allTasksAsString);
-    taskTitle.value = '';
-    taskDescription.value = '';
-    taskDueDate.value = '';
-    taskContact.value = '';
-    taskCategory.value = '';
+        createdAt: new Date().getTime(),
+    });
+    await setItem('task', JSON.stringify(allTasks));
+    loadAllTask();
 }
 
 
 // getItem()
 
-function loadAllTask() {
-    let allTasksAsString = localStorage.getItem('allTasks');
-    allTasks = JSON.parse(allTasksAsString);
+async function loadAllTask() {
+    try{
+        allTasks = JSON.parse(await getItem('task'));
+    } catch(e){
+        console.error('Loading error:', e);
+    }
 }
